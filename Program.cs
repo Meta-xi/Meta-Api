bea52015a7fc6cf1677402a697e8b10f8bcff9cf
Yasel <yaselbarrioscarrillo@gmail.com>
2024-11-19 12:26:09 -0500

    Arreglar decimales

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ builder.Services.AddSwaggerGen(c =>{
 });
 
 builder.Services.AddDbContext<DBContext>(options => 
-options.UseNpgsql("Host=autorack.proxy.rlwy.net;Port=56967;Username=postgres;Password=ZMnnjLFsLhKgshApPZQbgEASgGhUUQOC;Database=railway"
+options.UseNpgsql("Server=localhost;Port=5432;Database=Meta_xi;User ID=postgres;Password=1234"
 ));
 builder.Services.AddTransient<UserService, UserService>();
 builder.Services.AddScoped<IGeneratedJwt, GeneratedJwt>();
@@ -58,11 +58,6 @@ builder.Services.AddCors(option =>{
 });
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<DBContext>();
-    dbContext.Database.Migrate();
-}
 
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
