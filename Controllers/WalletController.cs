136491541d95ead44042fd2dd8dd98dd80e94ed6
PrivateWorkYasel <yaselbarrioscarrillo864@gmail.com>
2024-12-27 02:21:55 -0500

    modificar estado de referidos

--- Controllers/WalletController.cs
+++ Controllers/WalletController.cs
@@ -8,22 +8,57 @@ namespace Meta_xi.Application;
 public class WalletController : ControllerBase
 {
     private readonly DBContext context;
-    public WalletController(DBContext _context)
+    private readonly IBenefitPerRefer benefitPerRefer;
+    public WalletController(DBContext _context, IBenefitPerRefer _benefitPerRefer)
     {
         context = _context;
+        benefitPerRefer = _benefitPerRefer;
     }
     [HttpPost("UpdateBalance")]
     public async Task<IActionResult> UpdateBalance(UpdateBalance updateBalance)
     {
         GetMoneyValues getMoneyValues = new GetMoneyValues();
         var user = await context.Users.FirstOrDefaultAsync(option => option.Email == updateBalance.Email || option.PhoneNumber == updateBalance.Email);
-        if(user == null){
+        if (user == null)
+        {
             return NotFound(new { message = "No existe ninguna cuenta con ese correo" });
         }
         var wallet = await context.Wallets.FirstOrDefaultAsync(option => option.Email == updateBalance.Email);
         if (wallet == null)
         {
             return NotFound(new { message = "No existe ninguna cartera con ese correo" });
+        }
+        BenefitOperation benefitOperation = new BenefitOperation
+        {
+            PricePlan = updateBalance.Balance,
+            Username = updateBalance.Email
+        };
+        bool result3 = await benefitPerRefer.RegisterBenefitLevel3(benefitOperation);
+        if (!result3)
+        {
+            Console.WriteLine("No se actualizo para level3");
+        }
+        else
+        {
+            Console.WriteLine("Se actualizo para level3");
+        }
+        bool result2 = await benefitPerRefer.RegisterBenefitLevel2(benefitOperation);
+        if (!result2)
+        {
+            Console.WriteLine("No se actualizo para level2");
+        }
+        else
+        {
+            Console.WriteLine("Se actualizo para level2");
+        }
+        bool result = await benefitPerRefer.RegisterBenefitLevel1(benefitOperation);
+        if (!result)
+        {
+            Console.WriteLine("No se actualizo para level1");
+        }
+        else
+        {
+            Console.WriteLine("Se actualizo para level3");
         }
         string token = updateBalance.Token.ToLower();
         switch (token)
@@ -32,9 +67,10 @@ public class WalletController : ControllerBase
                 wallet.Balance += updateBalance.Balance;
                 context.Entry(wallet).State = EntityState.Modified;
                 await context.SaveChangesAsync();
-                RechargeLog rechargeLog = new RechargeLog{
+                RechargeLog rechargeLog = new RechargeLog
+                {
                     IdUser = user.Id,
-                    Recharge =updateBalance.Balance,
+                    Recharge = updateBalance.Balance,
                     Date = DateTime.UtcNow,
                     User = null
                 };
@@ -51,9 +87,10 @@ public class WalletController : ControllerBase
                 wallet.Balance = wallet.Balance + (value * updateBalance.Balance * usd);
                 context.Entry(wallet).State = EntityState.Modified;
                 await context.SaveChangesAsync();
-                RechargeLog rechargeLogtrx = new RechargeLog{
+                RechargeLog rechargeLogtrx = new RechargeLog
+                {
                     IdUser = user.Id,
-                    Recharge =updateBalance.Balance * value * usd,
+                    Recharge = updateBalance.Balance * value * usd,
                     Date = DateTime.UtcNow,
                     User = null
                 };
@@ -68,9 +105,10 @@ public class WalletController : ControllerBase
                 wallet.Balance = wallet.Balance + (value2 * updateBalance.Balance * usd2);
                 context.Entry(wallet).State = EntityState.Modified;
                 await context.SaveChangesAsync();
-                RechargeLog rechargeLogusdt_trc20 = new RechargeLog{
+                RechargeLog rechargeLogusdt_trc20 = new RechargeLog
+                {
                     IdUser = user.Id,
-                    Recharge =updateBalance.Balance,
+                    Recharge = updateBalance.Balance,
                     Date = DateTime.UtcNow,
                     User = null
                 };
@@ -83,9 +121,10 @@ public class WalletController : ControllerBase
                 wallet.Balance = wallet.Balance + (value3 * updateBalance.Balance);
                 context.Entry(wallet).State = EntityState.Modified;
                 await context.SaveChangesAsync();
-                RechargeLog rechargeLogpaypal = new RechargeLog{
+                RechargeLog rechargeLogpaypal = new RechargeLog
+                {
                     IdUser = user.Id,
-                    Recharge =updateBalance.Balance,
+                    Recharge = updateBalance.Balance,
                     Date = DateTime.UtcNow,
                     User = null
                 };
@@ -95,6 +134,8 @@ public class WalletController : ControllerBase
             default:
                 return NotFound(new { message = "Token no soportado" });
         }
+        
+
     }
     [HttpGet("GetBalance/{username}")]
     public async Task<IActionResult> GetBalance(string username)
@@ -107,22 +148,27 @@ public class WalletController : ControllerBase
         return Ok(wallet.Balance);
     }
     [HttpPatch("WithdrawBalance")]
-    public async Task<IActionResult> WithdrawalBalance(WitdrawBalance witdrawBalance){
+    public async Task<IActionResult> WithdrawalBalance(WitdrawBalance witdrawBalance)
+    {
         var user = await context.Users.FirstOrDefaultAsync(u => u.Email == witdrawBalance.Username || u.PhoneNumber == witdrawBalance.Username);
-        if(user == null){
+        if (user == null)
+        {
             return NotFound(new { message = "No existe ninguna cuenta con ese correo" });
         }
         var wallet = await context.Wallets.FirstOrDefaultAsync(u => u.Email == witdrawBalance.Username);
-        if(wallet == null){
+        if (wallet == null)
+        {
             return NotFound(new { message = "El usuario no posee ninguna cartera" });
         }
-        if(wallet.Balance < witdrawBalance.Withdraw){
+        if (wallet.Balance < witdrawBalance.Withdraw)
+        {
             return NotFound(new { message = "No hay suficiente saldo" });
         }
         wallet.Balance -= witdrawBalance.Withdraw;
         context.Entry(wallet).State = EntityState.Modified;
         await context.SaveChangesAsync();
-        WithdrawLog withdrawLog = new WithdrawLog{
+        WithdrawLog withdrawLog = new WithdrawLog
+        {
             Date = DateTime.UtcNow,
             User = null,
             Withdraw = witdrawBalance.Withdraw,
@@ -133,71 +179,88 @@ public class WalletController : ControllerBase
         return Ok(new { message = "Balance actualizado correctamente" });
     }
     [HttpGet("GetBalanceUsdAndCop/{username}")]
-    public async Task<IActionResult> GetBalanceUsdAndCop(string username){
+    public async Task<IActionResult> GetBalanceUsdAndCop(string username)
+    {
         var user = await context.Users.FirstOrDefaultAsync(u => u.Email == username || u.PhoneNumber == username);
-        if(user == null){
+        if (user == null)
+        {
             return NotFound(new { message = "Usuario no encontrado" });
         }
-        var wallet = await context.Wallets.FirstOrDefaultAsync(w => w.Email ==username);
-        if(wallet == null){
+        var wallet = await context.Wallets.FirstOrDefaultAsync(w => w.Email == username);
+        if (wallet == null)
+        {
             return NotFound(new { message = "Billetera no encontrada" });
         }
         GetMoneyValues getMoneyValues = new GetMoneyValues();
         float balanceToUsd = 0;
         decimal currenttlyBalance = await getMoneyValues.GetMoneyValueAsync("cop");
-        if(wallet.Balance > 0){
+        if (wallet.Balance > 0)
+        {
             balanceToUsd = wallet.Balance / (float)currenttlyBalance;
         }
-        GetUsdAndCop getUsdAndCop = new GetUsdAndCop{
-            BalanceInCop = (float)Math.Round(wallet.Balance , 2),
-            BalanceInUsd = (float)Math.Round(balanceToUsd , 2)
+        GetUsdAndCop getUsdAndCop = new GetUsdAndCop
+        {
+            BalanceInCop = (float)Math.Round(wallet.Balance, 2),
+            BalanceInUsd = (float)Math.Round(balanceToUsd, 2)
         };
         return Ok(getUsdAndCop);
     }
     [HttpGet("GetRechargeAndWithdraw/{username}")]
-    public async Task<IActionResult> GetRechargeAndWithdraw(string username){
-        var user = await context.Users.FirstOrDefaultAsync( u => u.Email == username || u.PhoneNumber == username);
-        if(user == null){
+    public async Task<IActionResult> GetRechargeAndWithdraw(string username)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Email == username || u.PhoneNumber == username);
+        if (user == null)
+        {
             return NotFound(new { message = "Usuario no encontrado" });
         }
         var recharge = await context.RechargeLogs.Where(u => u.IdUser == user.Id).SumAsync(u => u.Recharge);
         var withdraw = await context.WithdrawLogs.Where(u => u.UserId == user.Id).SumAsync(u => u.Withdraw);
-        GetRechargeAndWithdraw getParameters = new GetRechargeAndWithdraw{
+        GetRechargeAndWithdraw getParameters = new GetRechargeAndWithdraw
+        {
             Recharge = recharge,
             Withdraw = withdraw
         };
         return Ok(getParameters);
     }
     [HttpGet("ClaimWelcomeBonus/{username}")]
-    public async Task<IActionResult> ClaimWelcomeBonus(string username){
+    public async Task<IActionResult> ClaimWelcomeBonus(string username)
+    {
         var user = await context.Users.FirstOrDefaultAsync(u => u.Email == username || u.PhoneNumber == username);
-        if(user == null){
+        if (user == null)
+        {
             return NotFound(new { message = "Usuario no encontrado" });
         }
         var bonus = await context.WelcomeBonuss.FirstOrDefaultAsync(u => u.UserID == user.Id);
-        if(bonus == null){
+        if (bonus == null)
+        {
             return NotFound(new { message = "Bonus no encontrado" });
         }
-        if(bonus.IsClaimed){
+        if (bonus.IsClaimed)
+        {
             return NotFound(new { message = "Bonus ya ha sido reclamado" });
         }
         return Ok();
     }
     [HttpPost("ClaimWelcomeBonus")]
-    public async Task<IActionResult> ClaimWelcomeBonus( UpdateBalance updateBalance){
+    public async Task<IActionResult> ClaimWelcomeBonus(UpdateBalance updateBalance)
+    {
         var user = await context.Users.FirstOrDefaultAsync(u => u.Email == updateBalance.Email || u.PhoneNumber == updateBalance.Email);
-        if(user == null){
+        if (user == null)
+        {
             return NotFound(new { message = "Usuario no encontrado" });
         }
         var bonus = await context.WelcomeBonuss.FirstOrDefaultAsync(u => u.UserID == user.Id);
-        if(bonus == null){
+        if (bonus == null)
+        {
             return NotFound(new { message = "Bonus no encontrado" });
         }
-        if(bonus.IsClaimed){
+        if (bonus.IsClaimed)
+        {
             return NotFound(new { message = "Bonus ya ha sido reclamado" });
         }
         var wallet = await context.Wallets.FirstOrDefaultAsync(u => u.Email == updateBalance.Email);
-        if(wallet == null){
+        if (wallet == null)
+        {
             return NotFound(new { message = "Billetera no encontrada" });
         }
         wallet.Balance += updateBalance.Balance;
--- obj/Debug/net8.0/Meta-xi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Meta-xi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Meta-xi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf501f278fad06311e20e414b666ef3db657a0c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce3beb8c1fc0a65333ebc5952f1e0e44809b6c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Meta-xi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Meta-xi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
